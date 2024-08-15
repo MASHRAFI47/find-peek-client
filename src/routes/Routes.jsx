881eee49bb1275edb970b4f02713df79313f36be
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-15 21:46:29 +0600

    header home and footer added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
+import Root from '../layout/Root';
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
     },
 ]);
 
