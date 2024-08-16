17dde74b38b48dda3fe2b936ae6da49774e73038
MASHRAFI47 <mbngms@gmail.com>
2024-08-16 17:56:27 +0600

    search by category added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,8 +12,9 @@ const Products = () => {
 
     const [asc, setAsc] = useState(true);
     const [search, setSearch] = useState('');
+    const [category, setCategory] = useState('');
 
-    const products = useProducts(asc, search);
+    const products = useProducts(asc, search, category);
 
     // const axiosCommon = useAxiosCommon();
 
@@ -49,14 +50,37 @@ const Products = () => {
     return (
         <div>
 
-            <div className="container text-center mb-5">
+            <div className="w-full text-center mb-5">
                 <form action="" onSubmit={handleSearch}>
-                    <input type="text" placeholder="Search here" name="search" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Search By Product Name" name="search" className="input input-bordered w-full max-w-xs" />
                     <input type="submit" value={"Search"} className="btn btn-primary" />
                 </form>
-                <button className="btn" onClick={() => setAsc(!asc)}>
-                    {asc ? "Price: High to Low" : "Price: Low to High"}
+                <button className="btn btn-success mt-3" onClick={() => setAsc(!asc)}>
+                    {asc ? "Sort By: High to Low" : "Price: Low to High"}
                 </button>
+            </div>
+
+            <div className="container mx-auto">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-warning m-1">Brand</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li onClick={() => setSearch("")}><a>All</a></li>
+                        <li onClick={() => setSearch("MSI")}><a>MSI</a></li>
+                        <li onClick={() => setSearch("ASUS")}><a>ASUS</a></li>
+                    </ul>
+                </div>
+
+
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-warning m-1">Category</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li onClick={() => setCategory("")}><a>All</a></li>
+                        <li onClick={() => setCategory("Monitor")}><a>Monitor</a></li>
+                        <li onClick={() => setCategory("Headphone")}><a>Headphone</a></li>
+                        <li onClick={() => setCategory("Motherboard")}><a>Motherboard</a></li>
+                        <li onClick={() => setCategory("Casing")}><a>Casing</a></li>
+                    </ul>
+                </div>
             </div>
 
             <div className="container mx-auto grid grid-cols-4 gap-8">
--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
-const useProducts = (asc, search) => {
+const useProducts = (asc, search, category) => {
     const [products, setProducts] = useState([]);
     
     useEffect(() => {
-        fetch(`http://localhost:3000/products?sort=${asc ? 'asc' : 'desc'}&search=${search}`)
+        fetch(`http://localhost:3000/products?sort=${asc ? 'asc' : 'desc'}&search=${search}&category=${category}`)
             .then(res => res.json())
             .then(data => setProducts(data));
-    }, [asc, search]);
+    }, [asc, search, category]);
 
     return products;
 }
