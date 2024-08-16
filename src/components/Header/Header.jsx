05ab065c8f64aa66e9736cc716e430a0da58c465
MASHRAFI47 <mbngms@gmail.com>
2024-08-16 23:20:09 +0600

    login image added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom"
 import logo from '../../assets/images/findpeek.png';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     const links = <>
         <li><NavLink to={'/'}>Home</NavLink></li>
     </>
@@ -44,7 +47,32 @@ const Header = () => {
                     </div> */}
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    {
+                        user ?
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img
+                                            alt="Tailwind CSS Navbar component"
+                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                    <li>
+                                        <a className="justify-between">
+                                            Profile
+                                            <span className="badge">New</span>
+                                        </a>
+                                    </li>
+                                    <li><a>Settings</a></li>
+                                    <li><a>Logout</a></li>
+                                </ul>
+                            </div>
+                            :
+                            <a href="" className="btn">Login</a>
+                    }
                 </div>
             </div>
         </div>
