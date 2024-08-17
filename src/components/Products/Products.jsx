af283ce617347b0566c1c7e7d2a3929c6f356bc0
MASHRAFI47 <mbngms@gmail.com>
2024-08-17 09:45:41 +0600

    connections with api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -11,36 +11,52 @@ import axios from "axios";
 
 const Products = () => {
 
+    // eslint-disable-next-line no-unused-vars
     const [asc, setAsc] = useState(true);
     const [search, setSearch] = useState('');
+    // eslint-disable-next-line no-unused-vars
     const [category, setCategory] = useState('');
 
-    const [myProducts, setMyProducts] = useState([]);
 
-    const products = useProducts(asc, search, category);
+    const [myProducts, setMyProducts] = useState([]);
 
 
     //pagination state
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(3);
+    // eslint-disable-next-line no-unused-vars
+    const [itemsPerPage, setItemsPerPage] = useState(2);
     const [count, setCount] = useState(0);
+
+    //filter
+    const [filter, setFilter] = useState("");
+
+    // eslint-disable-next-line no-unused-vars
+    const [brand, setBrand] = useState("");
+
+    //sort
+    const [sort, setSort] = useState("");
+
+    // eslint-disable-next-line no-unused-vars
+    const products = useProducts(asc, search, category, currentPage, itemsPerPage);
+
+
 
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-data?page=${currentPage}&size=${itemsPerPage}`)
+            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-data?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}&brand=${brand}`)
             setMyProducts(data);
         }
         getData()
-    }, [currentPage, itemsPerPage]);
+    }, [currentPage, itemsPerPage, filter, sort, search, brand]);
 
 
     useEffect(() => {
         const getProductsCount = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-count`);
+            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-count?filter=${filter}&search=${search}`);
             setCount(data.count)
         }
         getProductsCount()
-    }, []);
+    }, [filter, search]);
 
     console.log(count)
 
@@ -68,13 +84,13 @@ const Products = () => {
                     <input type="text" placeholder="Search By Product Name" name="search" className="input input-bordered w-full max-w-xs" />
                     <input type="submit" value={"Search"} className="btn btn-primary" />
                 </form>
-                <button className="btn btn-success mt-3" onClick={() => setAsc(!asc)}>
+                {/* <button className="btn btn-success mt-3" onClick={() => setAsc(!asc)}>
                     {asc ? "Sort By: High to Low" : "Price: Low to High"}
-                </button>
+                </button> */}
             </div>
 
             <div className="container mx-auto mb-3">
-                <div className="dropdown">
+                {/* <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-warning m-1">Brand</div>
                     <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                         <li onClick={() => setSearch("")}><a>All</a></li>
@@ -93,15 +109,36 @@ const Products = () => {
                         <li onClick={() => setCategory("Motherboard")}><a>Motherboard</a></li>
                         <li onClick={() => setCategory("Casing")}><a>Casing</a></li>
                     </ul>
-                </div>
+                </div> */}
+
+                <select className="select select-bordered w-full max-w-xs" onChange={e => {
+                    setSort(e.target.value);
+                    setCurrentPage(1);
+                }} value={sort}>
+                    <option disabled selected>Sort By</option>
+                    <option value={"asc"}>Sort By (Ascending)</option>
+                    <option value={"dsc"}>Sort By (Descending)</option>
+                </select>
+
+                <select className="select select-bordered w-full max-w-xs" onChange={e => {
+                    setFilter(e.target.value);
+                    setCurrentPage(1);
+                }} value={filter}>
+                    <option disabled selected>Category</option>
+                    <option value={""}>Category</option>
+                    <option value={"Monitor"}>Monitor</option>
+                    <option value={"Headphone"}>Headphone</option>
+                    <option value={"Motherboard"}>Motherboard</option>
+                    <option value={"Casing"}>Casing</option>
+                </select>
             </div>
 
-            <div className="container mx-auto grid grid-cols-4 gap-8">
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 {
                     myProducts?.map(product => <div key={product._id}>
                         <div className="card bg-base-100 border shadow-xl">
                             <figure>
-                                <img className="w-64"
+                                <img className=""
                                     src={product?.productImage}
                                     alt="Shoes" />
                             </figure>
--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react"
 
-const useProducts = (asc, search, category) => {
+const useProducts = (asc, search, category, currentPage, itemsPerPage) => {
     const [products, setProducts] = useState([]);
     
     useEffect(() => {
-        fetch(`http://localhost:3000/products?sort=${asc ? 'asc' : 'desc'}&search=${search}&category=${category}`)
+        fetch(`http://localhost:3000/products?sort=${asc ? 'asc' : 'desc'}&search=${search}&category=${category}&page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => {
                 setProducts(data)
             });
-    }, [asc, search, category]);
+    }, [asc, search, category, itemsPerPage, currentPage]);
 
     return products;
 }
